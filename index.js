80b7912199d44c8ea14177a734939b7216c18579
ajspalma <palma.ashleyjs@gmail.com>
2019-12-16 09:55:57 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
+
+http.listen(3000, '192.168.4.56', function(){
+  console.log('listening on *:3000');
+});
